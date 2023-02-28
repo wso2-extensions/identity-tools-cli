ddfd4b535bc83438a8ab4fddd5006c63b0448549
Hasini Samarathunga <hasinis@wso2.com>
2023-02-28 12:24:27 +0530

    Added update functionality

--- src/cmd/importAll.go
+++ src/cmd/importAll.go
@@ -3,12 +3,20 @@ package cmd
 import (
 	"bytes"
 	"crypto/tls"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime"
 	"mime/multipart"
 	"net/http"
+	"net/textproto"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 var importAllCmd = &cobra.Command{
@@ -17,11 +25,10 @@ var importAllCmd = &cobra.Command{
 	Long:  `You can import all service providers`,
 	Run: func(cmd *cobra.Command, args []string) {
 		inputDirPath, _ := cmd.Flags().GetString("inputDir")
-		format, _ := cmd.Flags().GetString("format")
 		configFile, _ := cmd.Flags().GetString("config")
 
 		SERVER_CONFIGS = loadServerConfigsFromFile(configFile)
-		importAllApps(inputDirPath, format)
+		importAllApps(inputDirPath)
 	},
 }
 
@@ -29,11 +36,14 @@ func init() {
 
 	rootCmd.AddCommand(importAllCmd)
 	importAllCmd.Flags().StringP("inputDir", "i", "", "Path to the input directory")
-	importAllCmd.Flags().StringP("format", "f", "yaml", "Format of the imported files")
 	importAllCmd.Flags().StringP("config", "c", "", "Path to the config file")
 }
 
-func importAllApps(inputDirPath string, format string) {
+type AppConfig struct {
+	ApplicationName string `yaml:"applicationName"`
+}
+
+func importAllApps(inputDirPath string) {
 
 	var importFilePath = "."
 	if inputDirPath != "" {
@@ -47,39 +57,100 @@ func importAllApps(inputDirPath string, format string) {
 	}
 
 	var appFilePath string
+	appList := getAppNames()
+
 	for _, file := range files {
 		appFilePath = importFilePath + file.Name()
-		log.Println("Importing app: " + file.Name())
-		importApp(appFilePath, format)
+		fileName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+
+		// Read the content of the file.
+		fileContent, err := ioutil.ReadFile(appFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Parse the YAML content.
+		var appConfig AppConfig
+		err = yaml.Unmarshal(fileContent, &appConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(appConfig.ApplicationName)
+
+		// Check if app exists.
+		var appExists bool
+		for _, app := range appList {
+			if app == appConfig.ApplicationName {
+				appExists = true
+				break
+			}
+		}
+
+		if appConfig.ApplicationName != fileName {
+			log.Println("Application name in the file " + appFilePath + " is not matching with the file name.")
+			// log.Println("Renaming file name to " + appConfig.ApplicationName + ".yml")
+			// err := os.Rename(appFilePath, importFilePath+appConfig.ApplicationName+".yml")
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+
+			// appFilePath = importFilePath + appConfig.ApplicationName + ".yml"
+		}
+
+		importApp(appFilePath, appExists)
 	}
 }
 
-func importApp(importFilePath string, format string) {
+func importApp(importFilePath string, update bool) {
 
-	var reqUrl = SERVER_CONFIGS.ServerUrl + "/t/" + SERVER_CONFIGS.TenantDomain + "/api/server/v1/applications/import/" + format
+	var reqUrl = SERVER_CONFIGS.ServerUrl + "/t/" + SERVER_CONFIGS.TenantDomain + "/api/server/v1/applications/import"
 	var err error
 
-	fileBytes, err := ioutil.ReadFile(importFilePath)
+	// fileBytes, err := ioutil.ReadFile(importFilePath)
+	file, err := os.Open(importFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	extraParams := map[string]string{
-		"file": string(fileBytes),
-	}
+	filename := filepath.Base(importFilePath)
 
 	body := new(bytes.Buffer)
 	writer := multipart.NewWriter(body)
 
-	for key, val := range extraParams {
-		err := writer.WriteField(key, val)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// Get file extension
+	fileExtension := filepath.Ext(filename)
+
+	mime.AddExtensionType(".yml", "text/yaml")
+	mime.AddExtensionType(".xml", "application/xml")
+
+	mimeType := mime.TypeByExtension(fileExtension)
+
+	part, err := writer.CreatePart(textproto.MIMEHeader{
+		"Content-Disposition": []string{fmt.Sprintf(`form-data; name="%s"; filename="%s"`, "file", filename)},
+		"Content-Type":        []string{mimeType},
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer writer.Close()
 
-	request, err := http.NewRequest("POST", reqUrl, body)
+	var requestMethod string
+	if update {
+		log.Println("Updating app: " + filename)
+		requestMethod = "PUT"
+	} else {
+		log.Println("Importing app: " + filename)
+		requestMethod = "POST"
+	}
+
+	request, err := http.NewRequest(requestMethod, reqUrl, body)
 	request.Header.Add("Content-Type", writer.FormDataContentType())
 	request.Header.Set("Authorization", "Bearer "+SERVER_CONFIGS.Token)
 	defer request.Body.Close()
@@ -108,7 +179,8 @@ func importApp(importFilePath string, format string) {
 	case 403:
 		log.Println("Forbidden request.")
 	case 409:
-		log.Println("An application with the same name already exists.")
+		log.Println("An application with the same name already exists. Please rename the file accordingly.")
+		importApp(importFilePath, true)
 	case 500:
 		log.Println("Internal server error.")
 	case 201:
