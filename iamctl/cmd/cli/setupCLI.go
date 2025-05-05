d9bd38c51777ca10fd1b0a67b9dc9f321e2f7cf0
Hasini Samarathunga <hasinis@wso2.com>
2025-05-05 10:31:54 +0530

    Add review suggestions

--- iamctl/cmd/cli/setupCLI.go
+++ iamctl/cmd/cli/setupCLI.go
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
+* Copyright (c) 2023-2025, WSO2 LLC. (https://www.wso2.com).
 *
 * WSO2 LLC. licenses this file to you under the Apache License,
 * Version 2.0 (the "License"); you may not use this file except
@@ -87,7 +87,7 @@ func createConfigFolder(baseDirPath string) {
 	defer file.Close()
 
 	// Create keyword config directory.
-	file, err = os.OpenFile(envConfigDir+utils.KEYWORD_CONFIG_FILE, os.O_CREATE, 0644)
+	file, err = os.OpenFile(envConfigDir + utils.KEYWORD_CONFIG_FILE, os.O_CREATE, 0644)
 	if err != nil {
 		log.Println("Error in creating the keyword config file", err)
 	}
